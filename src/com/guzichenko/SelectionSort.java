993eada753e9204532c3aed9586d4ac26c52f468
Artem <you@example.com>
2019-02-04 20:13:50 +0200

    selection sort

---
+++ src/com/guzichenko/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.guzichenko;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int[] ints = {5, 7, 1, 3, 2, 0};
+		sort(ints);
+	}
+
+	public static void sort(int[] ints) {
+		for (int i = 0; i < ints.length; i++) {
+			for (int j = 0; j < ints.length; j++) {
+				if (ints[i] > ints[j]) {
+					int a = ints[j];
+					ints[j] = ints[i];
+					ints[i] = a;
+				}
+			}
+		}
+
+		System.out.println(Arrays.toString(ints));
+
+	}
+
+}
